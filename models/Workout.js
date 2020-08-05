2af6e5e4da11025b36c639152396fc49e72d32fa
Kyle Rudderforth <kruddzz@gmail.com>
2020-08-04 21:35:41 -0500

    api routes started and path added to the server.js

--- models/Workout.js
+++ models/Workout.js
@@ -1,10 +1,43 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-  title: String,
-  body: String
-});
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: () => new Date()
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name"
+        },
+        distance: {
+          type: Number
+        },
+        duration: {
+          type: Number,
+        },
+        weight: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        } 
+      }
+    ]
+  }
+);
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-// const db = require("./models");
-
 const app = express();
 
 app.use(logger("dev"));
@@ -15,9 +13,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", {
+   useNewUrlParser: true,
+   useFindAndModify: false
+  });
 
-
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
