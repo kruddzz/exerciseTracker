e670780adaeefd9f5613533eed72069690fb6e6b
Kyle Rudderforth <kruddzz@gmail.com>
2020-08-04 22:01:48 -0500

    find route added

--- routes/api.js
+++ routes/api.js
@@ -24,5 +24,16 @@ router.post("/api/workouts", (req, res) => {
         res.json(err);
       });
   });
+  
+  router.get("/api/workouts", (req, res) => {
+    Workout.find()
+      .then(dbWorkouts => {
+        res.json(dbWorkouts);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
 
 module.exports = router;
