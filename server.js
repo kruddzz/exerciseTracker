ad36decb7c4223ddae9e94ba3baebb349d022716
Kyle Rudderforth <kruddzz@gmail.com>
2020-08-14 09:46:01 -0500

    changes to touter

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
+const apiRoutes = require("./routes/api")
+const htmlRoutes= require("./routes/html")
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -19,8 +20,8 @@ mongoose.connect(process.env.MONGOATLAS_URI || "mongodb://localhost/fitnessTrack
   });
 
 // Routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"));
+app.use(apiRoutes);
+app.use(htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
